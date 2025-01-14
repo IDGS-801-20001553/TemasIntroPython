1fbbf01345920b93183ae4482eb7d4a6a8901393
IDGS-801-20001553 <junonyu1234@gmail.com>
2025-01-13 23:51:34 -0600

    primer commit

---
+++ 03-cadenas.py
@@ -0,0 +1,28 @@
+texto1="hola"
+texto2="mundo"
+textoFiuna=texto1+" "+texto2
+print(textoFiuna)
+
+print("el saludo es : %s %s" %(texto1, texto2))
+
+saludoFinal0="saludo: {x}  {y}".format(x=texto1,y=texto2)
+print(saludoFinal0)
+
+texto="desarrollo web profesional utl"
+
+print(texto)
+print(texto.lower())
+print(texto.upper())
+print(texto.title())
+print(texto.find("al"))
+print(texto.count("e"))
+
+print(texto.replace("e","a"))
+cadenaSeparada=texto.split(" ")
+print(cadenaSeparada)
+
+
+
+
+
+
